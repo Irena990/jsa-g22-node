74c55e3f5642705a36457f6b1d42dd1cbef38095
Irena <irena.mencovska@gmail.com>
2023-02-26 00:02:34 +0100

    Third commit

---
+++ c4/index.js
@@ -0,0 +1,17 @@
+// student [ime, prezime, prosek, grad]
+
+// GRUD
+// dodavanje na student vo fajlot
+// brisenje na student od fajlot
+// menuvanje na podatoci na student od fajlot
+// citanje na site studenti od fajlot
+/*- treba da dodademe student vo fajlot 
+
+1. Go dodavame studentot AAA vo fajlot
+    - Imeto na studentot treba da bide smeneto od AAA vo AAB
+2.1 za da moze da se napravi promena, prvo treba da se vcita celata sodrzina od fajlot
+2.2 da se konvertira sodrzinata od obicen tekst vo JS niza / objekt
+2.3 da se izminat site elementi vo nizata i da se napravi promena samo na sodvetniot clen
+2.4 nizata od JS niza/objekt treba da bide konvertirana vo tekst
+2.5 tekstot treba da bide zacuvan vo fajlot*/
+
